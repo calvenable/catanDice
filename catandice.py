612073a7cf11939c6d64ae72eb1680156ca60dbe
calvenable <63717251+calvenable@users.noreply.github.com>
2022-04-14 21:47:45 +0100

    Visual update

--- catandice.py
+++ catandice.py
@@ -8,7 +8,49 @@ print("  remade and you can continue playing, safe in the knowledge that every r
 print("turn up exactly the number of times you want it to within 36 turns. Happy settling!")
 print("                       Type 'quit' to exit gracefully.\n")
 
+# •, ⦾, and ⦿ are possibilities but not ASCII/Terminal friendly
 
+diceImages = [
+  [
+    "|       |",
+    "|   O   |",
+    "|       |"
+  ],
+  [
+    "| O     |",
+    "|       |",
+    "|     O |"
+  ],
+  [
+    "|     O |",
+    "|   O   |",
+    "| O     |"
+  ],
+  [
+    "| O   O |",
+    "|       |",
+    "| O   O |"
+  ],
+  [
+    "| O   O |",
+    "|   O   |",
+    "| O   O |"
+  ],
+  [
+    "| O   O |",
+    "| O   O |",
+    "| O   O |"
+  ]
+]
+
+turnCounter = 1
+
+def populateArrayWithPairs():
+  arr = []
+  for i in range(1,7):
+    for j in range(1,7):
+      arr.append([i,j])
+  return arr
 
 def populateArray():
   arr = []
@@ -17,26 +59,54 @@ def populateArray():
       arr.append(i+j)
   return arr
 
+def printDiceImage(die1, die2):
+  print("\n   =======     ======= ")
+  print("  " + diceImages[die1-1][0] + "   " + diceImages[die2-1][0])
+  print("  " + diceImages[die1-1][1] + "   " + diceImages[die2-1][1])
+  print("  " + diceImages[die1-1][2] + "   " + diceImages[die2-1][2])
+  print("   =======     ======= \n")
+
+def rollDicePair(dicePairArray):
+  choice = random.choice(dicePairArray)
+  sum = choice[0] + choice[1]
+
+  printDiceImage(choice[0], choice[1])
+
+  # Determine the correct determiner ('a' or 'an')
+  if (sum == 8 or sum == 11):
+    determiner = "an"
+  else:
+    determiner = "a"
+
+  if turnCounter < 37:
+    # Just print turnCounter
+    print("[" + str(turnCounter) + "] You rolled " + determiner + " " + str(sum) + "\n")
+  else:
+    # Print turnCounter and current deck progress
+    print("[" + str(turnCounter) + "/"+ str(37-dicePairArray.__len__()) + "] You rolled " + determiner + " " + str(sum) + "\n")
+  dicePairArray.remove(choice)
+
 def rollDice(diceArray):
   choice = random.choice(diceArray)
   print("You rolled: " + str(choice) + "\n")
   diceArray.remove(choice)
 
+
+
 # Program start
-remainingDieRolls = populateArray()
+remainingDieRolls = populateArrayWithPairs()
 #print(remainingDieRolls)
 
 while input("Press Enter to roll the dice...") != "quit":
 
   if (remainingDieRolls.__len__() > 0):
     # Regular roll
-    rollDice(remainingDieRolls)
+    rollDicePair(remainingDieRolls)
 
   else:
     # Reshuffle deck and roll
     print("Reshuffling the deck...")
-    remainingDieRolls = populateArray()
-    rollDice(remainingDieRolls)
-
-
-
+    remainingDieRolls = populateArrayWithPairs()
+    rollDicePair(remainingDieRolls)
+  
+  turnCounter += 1
